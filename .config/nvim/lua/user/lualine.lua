1c37d463b2788aa9a898c1b5f56b273ff2b339c8
Gabriel Marafon <gabriel@scirons.com>
2023-06-04 16:11:26 +0200

    Update lualine

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -1,6 +1,10 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+	return
+end
 
 local colors = {
-  orange   = '#d17b49',
+  red      = '#d17b49',
   yellow   = '#af865a',
   black    = '#1f1f1f',
   grey     = '#4A3637',
@@ -12,42 +16,59 @@ local colors = {
 local scyrons_theme = {
   normal = {
     a = { fg = colors.black, bg = colors.cyan },
-    b = { fg = colors.black, bg = colors.cyan },
-    c = { fg = colors.white, bg = colors.grey },
+    b = { fg = colors.black, bg = colors.cyan, },
+    c = { fg = colors.white, bg = colors.black},
+    y = { fg = colors.black, bg = colors.cyan, gui = 'bold' },
+    z = { fg = colors.black, bg = colors.cyan},
 
   },
 
-  insert  = { a = { fg = colors.black, bg = colors.orange } },
-  visual  = { a = { fg = colors.black, bg = colors.white  } },
-  replace = { a = { fg = colors.black, bg = colors.yellow } }
-  },
+  insert =  { a = { fg = colors.black, bg = colors.red,    gui = 'bold' } },
+  visual =  { a = { fg = colors.black, bg = colors.white,  gui = 'bold' } },
+  replace = { a = { fg = colors.black, bg = colors.yellow, gui = 'bold' } },
+
+--  inactive = {
+--    a = { fg = colors.white, bg = colors.black },
+--    b = { fg = colors.white, bg = colors.black },
+--    c = { fg = colors.black, bg = colors.black },
+
+}
+
+-- cool function for progress
+local progress = function()
+	local current_line = vim.fn.line(".")
+	local total_lines = vim.fn.line("$")
+	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	local line_ratio = current_line / total_lines
+	local index = math.ceil(line_ratio * #chars)
+	return chars[index]
+end
 
 require('lualine').setup {
-    options = {
+  options = {
     theme = scyrons_theme,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
+    icons_enabled = true,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '|', right = '|'},
+    disabled_filetypes = {},
+    always_divide_middle = true,
   },
-  sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
-    },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
+ sections = {
+    lualine_a = {'mode'},
+    lualine_b = {"%{&spell ? '[en_US.utf8]' : '[NoSpell]'}"},
+    lualine_c = {'branch','filename'},
+    lualine_x = {'fileformat', 'filetype', 'diff', 'diagnostics'},
+    lualine_y = { progress, 'progress'  },
+    lualine_z = {'location'}
   },
-  inactive_sections = {
-    lualine_a = { 'filename' },
+  inactive = {
+    lualine_a = {},
     lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
+--    lualine_c = {'filename'},
+--    lualine_x = {'location'},
     lualine_y = {},
-    lualine_z = { 'location' },
+    lualine_z = {}
   },
   tabline = {},
-  extensions = {},
+  extensions = {}
 }
